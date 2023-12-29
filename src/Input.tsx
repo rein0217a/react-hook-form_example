86fdb478d861ed861608d44f6c1c93150198dba6
Reina <rein0217a@icloud.com>
2023-12-30 00:56:51 +0900

    refactor: Inputを分離

---
+++ src/Input.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import {
+  useController,
+  useFormContext,
+} from 'react-hook-form';
+
+export const Input = ({
+  name,
+  placeholder,
+}: {
+  name: string;
+  placeholder: string;
+}) => {
+  // focus中か制御
+  const [isFocus, setIsFocus] = useState(false);
+  const { register, control } = useFormContext();
+  const {
+    formState: { errors },
+    fieldState: { isTouched }, // 一回でもinputにフォーカスしたか否か
+  } = useController({
+    name,
+    control,
+  });
+
+  const error = errors[name];
+
+  return (
+    <div>
+      <label htmlFor={name}>First Name</label>
+      {error && isFocus && <p>focus{error.message as string}</p>}
+      <input
+        placeholder={placeholder}
+        {...register(name, {
+          required: true,
+          onChange: () => setIsFocus(true),
+          onBlur: () => setIsFocus(false),
+        })}
+        onFocus={() => setIsFocus(true)} // focus中か制御
+      />
+      {error && isTouched && !isFocus && (
+        <p>not focus{error.message as string}</p>
+      )}
+    </div>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,77 +1,40 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import { useController, useForm } from 'react-hook-form';
+import { FormProvider, useForm } from 'react-hook-form';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 
 import './styles.css';
+import { Input } from './Input';
 
 function App() {
-  // focus中か制御
-  const [isFocus, setIsFocus] = useState(false);
   const schema = z.object({
-    firstName: z.string().min(1, { message: 'firstName Required' }),
-    lastName: z.string().min(1, { message: 'lastName Required' }),
+    firstName: z
+      .string()
+      .min(1, { message: 'firstName Required' })
+      .max(2, 'firstName max 2'),
+    lastName: z
+      .string()
+      .min(1, { message: 'lastName Required' })
+      .max(5, 'lastName max 5'),
   });
-  const { register, control, handleSubmit } = useForm({
+  const methods = useForm({
     mode: 'all', // 	Validation is triggered on both blur and change events.
     resolver: zodResolver(schema),
   });
-  const {
-    formState: { errors },
-    fieldState: { isTouched }, // 一回でもinputにフォーカスしたか否か
-  } = useController({
-    name: 'firstName',
-    control,
-  });
-  const onSubmit = (data:unknown) => {
+  const onSubmit = (data: unknown) => {
     alert(JSON.stringify(data));
   };
 
   return (
     <div className='App'>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <label htmlFor='firstName'>First Name</label>
-          {errors.firstName && isFocus && (
-            <p>focus {errors.firstName.message as string}</p>
-          )}
-          <input
-            placeholder='bill'
-            {...register('firstName', {
-              required: true,
-              maxLength: 2,
-              onChange: () => setIsFocus(true),
-              onBlur: () => setIsFocus(false),
-            })}
-            onFocus={() => setIsFocus(true)} // focus中か制御
-          />
-          {errors.firstName && isTouched && !isFocus && (
-            <p>not focus{errors.firstName.message  as string}</p>
-          )}
-        </div>
-
-        <div>
-          <label htmlFor='lastName'>Last Name</label>
-          <input
-            placeholder='luo'
-            {...register('lastName', { required: true })}
-          />
-          {errors.lastName && <p>{errors.lastName.message  as string}</p>}
-        </div>
-
-        <div>
-          <label htmlFor='email'>Email</label>
-          <input
-            {...register('email', {
-              required: true,
-            })}
-            placeholder='bluebill1049@hotmail.com'
-          />
-          {errors.email && <p>This is required</p>}
-        </div>
-        <input type='submit' />
-      </form>
+      <FormProvider {...methods}>
+        <form onSubmit={methods.handleSubmit(onSubmit)}>
+          <Input name={'firstName'} placeholder='firstName' />
+          <Input name={'lastName'} placeholder='lastName' />
+          <input type='submit' />
+        </form>
+      </FormProvider>
     </div>
   );
 }
