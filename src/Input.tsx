e8c0c3de5371ccccc3206e0014abf036ac8fbcaf
Reina <rein0217a@icloud.com>
2024-01-04 15:33:22 +0900

    refactor: react-hook-formの使用する関数等を変更

--- src/Input.tsx
+++ src/Input.tsx
@@ -1,8 +1,5 @@
 import React, { useState } from 'react';
-import {
-  useController,
-  useFormContext,
-} from 'react-hook-form';
+import { useController, useFormContext } from 'react-hook-form';
 
 export const Input = ({
   name,
@@ -13,31 +10,32 @@ export const Input = ({
 }) => {
   // focus中か制御
   const [isFocus, setIsFocus] = useState(false);
-  const { register, control } = useFormContext();
+  const { control } = useFormContext();
   const {
-    formState: { errors },
-    fieldState: { isTouched }, // 一回でもinputにフォーカスしたか否か
+    field,
+    fieldState: { error, isTouched }, // 一回でもinputにフォーカスしたか否か
   } = useController({
     name,
     control,
+    rules:{
+      required: true,
+      onChange: () => setIsFocus(true),
+      onBlur: () => setIsFocus(false),
+    }
   });
-
-  const error = errors[name];
 
   return (
     <div>
       <label htmlFor={name}>First Name</label>
       <div className='input_wrapper'>
-      {error && isFocus && <p className='error-tooltip'>focus{error.message as string}</p>}
-      <input
-        placeholder={placeholder}
-        {...register(name, {
-          required: true,
-          onChange: () => setIsFocus(true),
-          onBlur: () => setIsFocus(false),
-        })}
-        onFocus={() => setIsFocus(true)} // focus中か制御
-      />
+        {error && isFocus && (
+          <p className='error-tooltip'>focus{error.message as string}</p>
+        )}
+        <input
+          placeholder={placeholder}
+          {...field}
+          onFocus={() => setIsFocus(true)} // focus中か制御
+        />
       </div>
       {error && isTouched && !isFocus && (
         <p>not focus{error.message as string}</p>
